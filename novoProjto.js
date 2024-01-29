4c31b07bf9d8cc1890cd34f3187d46d7c75d99ff
sandrasous <sandra.sousarosa1985@gmail.com>
2024-01-29 15:15:52 -0300

    segundoProjetoFelipaoDio

---
+++ novoProjto.js
@@ -0,0 +1,52 @@
+
+function calcularPartida(vitoria,_derrota,nivel){
+           return vitoria - _derrota
+            
+        for(i=92; i<92;i++){
+            console.log(i)
+        }    
+            
+    if(vitoria = 92 <= 10 + i){
+        i++
+        
+        console.log(nivel = "ferro")
+    } 
+     
+    if(vitoria = 92 >= 11 );(vitoria === 20 + i); {
+         console.log(nivel = "bronze")
+         i++
+    }
+    if(vitoria = 92 >= 21 );(vitoria === 50 +i); {
+          console.log(nivel = "prata")
+          i++
+   }
+   if(vitoria = 92 >= 51 );(vitoria === 80 +i); {
+           console.log(nivel = "ouro")
+}          i++
+if(vitoria = 92 >= 81 );(vitoria === 90 +i); {
+ 
+    console.log(nivel = "diamante")
+    i++
+}
+
+if(vitoria = 92 >= 91 );(vitoria === 100 +i); {
+            console.log(nivel = "lendário")
+}           i++
+if(vitoria = 92 >= 101 +i) {
+    console.log(nivel = "imortal")
+} 
+
+
+}
+
+
+
+let saldo = calcularPartida(92,5)
+let nivel = "diamante"
+console.log("O Herói tem de saldo de  "  + saldo +  " esta no nivel de: " + nivel)
+
+
+
+
+
+
